24d9cca452a3d382b689f3b6c4c1d842311f1f60
Pier Fumagalli <pier@usrz.com>
2023-04-18 13:55:13 +0200

    Minimal build file

---
+++ build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -e
+
+# Root ourselves in our project directory and figure out our version
+cd "$(dirname "$0")"
+PATH="${PATH}:${PWD}/node_modules/.bin"
+VERSION="$(node -p 'require("./package.json").version')"
+
+# Wipe the dist directory prior rebuilding
+echo -e '\033[38;5;69m*\033[0m Removing "dist" directory...'
+rm -rf "dist/"
+
+# Check types and prepare our exported .d.ts
+echo -e '\033[38;5;69m*\033[0m Checking and generating types...'
+tsc -p "./tsconfig.json"
+tsc -p "./tsconfig-tsd.json"
+
+# Copy the "yargs-parser" types in our bundle
+echo -e '\033[38;5;69m*\033[0m Incorporating "yargs-parser" types...'
+cp "./node_modules/@types/yargs-parser/index.d.ts" "./dist/parser.d.mts"
+
+# Run ESLint on our sources
+echo -e '\033[38;5;69m*\033[0m Linting sources...'
+eslint 'src/**'
+
+# Compile our "ts-loader" loader and CLI
+echo -e '\033[38;5;69m*\033[0m Transpiling sources...'
+exec esbuild \
+	--platform=node \
+	--format=esm \
+	--target=node18 \
+	--outdir=./dist \
+	--sourcemap=inline \
+	--sources-content=false \
+	--out-extension:.js=.mjs \
+	--external:esbuild \
+	--define:__version="'${VERSION}'" \
+	--bundle \
+		src/*.mts
