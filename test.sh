ca3eecffb1379817430b36d940f5afb0f6620e44
Pier Fumagalli <pier@usrz.com>
2023-12-07 01:59:17 +0100

    Better output of tests and test forking

--- test.sh
+++ test.sh
@@ -3,35 +3,53 @@
 BASE="${PWD}/test/test"
 
 _cleanup() {
-  RV=$?
-  test $RV -eq 0 \
+  RV="$?"
+  test "${RV}" -eq 0 \
     && echo -e '\033[38;5;76m*\033[0m Test Success' \
     || echo -e '\033[38;5;203m*\033[0m Test Failed'
-  exit $RV
+  exit "${RV}"
 }
 
 _run() {
-  echo -e '\033[38;5;240m  *\033[0m' node "${@}" > /dev/stderr
-  exec node "${@}"
+  EXPECTED="$1"
+  shift
+
+  echo -e '  \033[38;5;240m*\033[0m' node ./dist/tsrun.mjs "${@}" > /dev/stderr
+  ACTUAL="$(node ./dist/tsrun.mjs "${@}")"
+
+  if test "${EXPECTED}" != "${ACTUAL}" ; then
+    echo -e '    \033[38;5;203m*\033[0m Expected:' "${EXPECTED}"
+    echo -e '    \033[38;5;203m*\033[0m   Actual:' "${ACTUAL}"
+    return 1
+  else
+    return 0
+  fi
 }
 
 trap "_cleanup" EXIT
 
+FAIL=0
+
 echo -e '\033[38;5;69m*\033[0m Testing with node' $(node -v)
 
 echo -e '\033[38;5;69m*\033[0m Testing "ts" file...'
-test "$(_run ./dist/tsrun.mjs ./test/test.ts)" == "file://${BASE}.ts" || exit 1
-test "$(_run ./dist/tsrun.mjs --force-esm ./test/test.ts)" == "file://${BASE}.ts" || exit 1
-test "$(_run ./dist/tsrun.mjs --force-cjs ./test/test.ts)" == "${BASE}.ts" || exit 1
+_run "file://${BASE}.ts" ./test/test.ts || FAIL=1
+_run "file://${BASE}.ts" --force-esm ./test/test.ts || FAIL=1
+_run "${BASE}.ts"        --force-cjs ./test/test.ts || FAIL=1
 
 echo -e '\033[38;5;69m*\033[0m Testing "mts" file...'
-test "$(_run ./dist/tsrun.mjs ./test/test.mts)" == "file://${BASE}.mts" || exit 1
-test "$(_run ./dist/tsrun.mjs --force-esm ./test/test.mts)" == "file://${BASE}.mts" || exit 1
-test "$(_run ./dist/tsrun.mjs --force-cjs ./test/test.mts)" == "file://${BASE}.mts" || exit 1
+_run "file://${BASE}.mts" ./test/test.mts || FAIL=1
+_run "file://${BASE}.mts" --force-esm ./test/test.mts || FAIL=1
+_run "file://${BASE}.mts" --force-cjs ./test/test.mts || FAIL=1
 
 echo -e '\033[38;5;69m*\033[0m Testing "cts" file...'
-test "$(_run ./dist/tsrun.mjs ./test/test.cts)" == "${BASE}.cts" || exit 1
-test "$(_run ./dist/tsrun.mjs --force-esm ./test/test.cts)" == "${BASE}.cts" || exit 1
-test "$(_run ./dist/tsrun.mjs --force-cjs ./test/test.cts)" == "${BASE}.cts" || exit 1
+_run "${BASE}.cts" ./test/test.cts || FAIL=1
+_run "${BASE}.cts" --force-esm ./test/test.cts || FAIL=1
+_run "${BASE}.cts" --force-cjs ./test/test.cts || FAIL=1
 
-exit 0
+echo -e '\033[38;5;69m*\033[0m Testing forking scripts...'
+_run "file://${BASE}.ts" ./test/forking.ts || FAIL=1
+_run "file://${BASE}.ts" --force-esm ./test/forking.ts || FAIL=1
+_run "${BASE}.ts"        --force-cjs ./test/forking.ts || FAIL=1
+
+exit "${FAIL}"
